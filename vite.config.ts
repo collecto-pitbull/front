f0d549df5bbfbec4f729ddef43128be6b310ccd2
shinso-raven <1116252@est.intec.edu.do>
2025-04-26 06:07:25 -0400

    fix: cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     proxy: {
       "/api": {
         target: "http://localhost:5072", // Replace with your server's URL
